efb9fc8d6563c2aa4db8b96d27ff5ca6d20e2e85
Deepak Guruswamy <deepak.kamesh@gmail.com>
2018-12-08 13:12:43 -0800

    separated world and robot

--- run_gmapping.sh
+++
@@ -1,7 +0,0 @@
-rosrun gmapping slam_gmapping \
-        scan:=sonny/laser_scan \
-        _base_frame:=sonny/base \
-        _odom_frame:=sonny/odomFrame \
-        _linearUpdate:=0.0 \
-        _angularUpdate:=0.0
-
