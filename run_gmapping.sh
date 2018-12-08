94dd40ca4c23603f9d229fa2698d10b7a61cf131
Deepak Guruswamy <deepak.kamesh@gmail.com>
2018-12-07 22:59:09 -0800

    Added rviz and gmapping sh

---
+++ run_gmapping.sh
@@ -0,0 +1,7 @@
+rosrun gmapping slam_gmapping \
+        scan:=sonny/laser_scan \
+        _base_frame:=sonny/base \
+        _odom_frame:=sonny/odomFrame \
+        _linearUpdate:=0.0 \
+        _angularUpdate:=0.0
+
